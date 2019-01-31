fab21ac74897a92eb8991bd4d0b95c438e1bad68
Aymeric Hays <aymhce@gmail.com>
2019-01-31 14:47:28 +0100

    version 3.4 arm

--- specific-home-script.sh
+++ specific-home-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export SIMPLE_VERSION=3.3
-export FULL_VERSION=${SIMPLE_VERSION}.0-1
+export SIMPLE_VERSION=3.4
+export FULL_VERSION=${SIMPLE_VERSION}.2.2-1
 export DOCKER_FILE=Dockerfile_ARMV7
 
 bash specific-home-script_just-build.sh
